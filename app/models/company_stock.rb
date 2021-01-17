b0ae056696399187ddf8bfb02467c068ef8927b4
Bruce Dickey <brucedickey@email.com>
2021-01-17 16:26:49 -0700

    Add display of stock prices on the UI via AJAX.

--- app/models/company_stock.rb
+++ app/models/company_stock.rb
@@ -4,15 +4,34 @@ require 'json'
 class CompanyStock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol) 
-    params    = { :access_key => Rails.application.credentials.market_stack[:api_key], 
-                  :symbols => "#{ticker_symbol}",
-                  :limit => 1 
-                }
-    uri       = URI('http://api.marketstack.com/v1/eod')
+    company_info = get_data('tickers', ticker_symbol)
+    stock_prices = get_data('eod',     ticker_symbol)
+
+    if company_info && stock_prices
+      new(ticker: ticker_symbol, name: company_info['name'], last_price: stock_prices['close'])
+    else 
+      nil
+    end
+  end
+
+  private
+
+  def self.get_data(endpoint, ticker_symbol)
+    params = { 
+      :access_key => Rails.application.credentials.market_stack[:api_key], 
+      :symbols => "#{ticker_symbol}",
+      :limit => 1 
+    }
+    path      = endpoint.eql?('eod')? "eod" : "tickers/#{ticker_symbol}"
+    uri       = URI("http://api.marketstack.com/v1/#{path}")
     uri.query = URI.encode_www_form(params)
     json      = Net::HTTP.get(uri)
-    data      = JSON.parse(json)['data'].first
+    full_data = JSON.parse(json)
 
-    closing_price = data['close']
+    begin    
+       ret_data  = endpoint.eql?('eod')? full_data['data'].first : full_data
+    rescue => exception
+      nil
+    end
   end
 end
