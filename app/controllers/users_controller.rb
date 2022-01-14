47ab87793604a751b61d7668e2287806399014fd
Bruce Dickey <brucedickey@email.com>
2022-01-14 11:31:31 -0700

    Add show for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,21 @@ class UsersController < ApplicationController
   end
 
   def my_portfolio
-    @tracked_stocks = current_user.company_stocks
+    @user           = current_user
+    @tracked_stocks = get_latest_stock_prices(current_user.company_stocks)
   end
 
   def search 
     if params[:friend].present?
-      all_matches    = User.friend_lookup(params[:friend])
-      friend_matches = all_matches.reject { |x| x.id == current_user.id }
+      @friends = User.friend_lookup(current_user, params[:friend])
 
-      @friends = friend_matches
-
-      if @friends
+      if !@friends.blank?
         respond_to do |format|
           format.js { render partial: 'users/friend_result' }
         end 
       else 
         respond_to do |format|
-          flash.now[:alert] = "The name #{params[:friend]} was not found"
+          flash.now[:alert] = "The friend was not found"
           format.js { render partial: 'users/friend_result' }
         end
       end
@@ -30,5 +28,18 @@ class UsersController < ApplicationController
         format.js { render partial: 'users/friend_result' }
       end
     end 
+  end
+
+  def show 
+    @user           = User.find(params[:id]) 
+    stocks          = @user.user_stocks 
+    @tracked_stocks = stocks.map { |stock| CompanyStock.find(stock.company_stock_id) }
+    @tracked_stocks = get_latest_stock_prices(@tracked_stocks)
   end  
+
+  private
+
+  def get_latest_stock_prices(tracked_stocks) 
+    tracked_stocks.each { |stock| stock.last_price = CompanyStock.price_lookup(stock.ticker) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@
 # see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  resources :user_stocks,  only: [:create, :destroy]
-  resources :user_friends, only: [:create, :destroy]
-
   devise_for :users
+  resources :users, only: [:show]  
 
   root 'welcome#index'
 
   get 'my_portfolio',  to: 'users#my_portfolio'
   get 'search_stock',  to: 'company_stocks#search'
+  resources :user_stocks,  only: [:create, :destroy]
 
   get 'my_friends',    to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
+  resources :user_friends, only: [:create, :destroy]
 end
